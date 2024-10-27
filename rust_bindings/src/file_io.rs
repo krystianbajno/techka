21c76203d588d79283c035b59d0abc1177a51f06
Krystian Bajno <krystian.bajno@gmail.com>
2024-10-27 19:12:42 +0100

    Initial commit

--- rust_bindings/src/file_io.rs
+++ rust_bindings/src/file_io.rs
@@ -1,19 +1,4 @@
 use anyhow::Result;
-use crate::models::MetadataEntry;
-use std::fs;
-use std::path::Path;
-
-pub fn load_metadata(filepath: &str) -> Result<Vec<MetadataEntry>> {
-    let metadata: Vec<MetadataEntry> = serde_json::from_str(&fs::read_to_string(filepath)?)?;
-    Ok(metadata)
-}
-
-pub fn write_output(data: &Vec<MetadataEntry>, output_path: &str) -> Result<()> {
-    let output_json = serde_json::to_string_pretty(data)?;
-    fs::create_dir_all(Path::new(output_path).parent().unwrap())?;
-    fs::write(output_path, output_json)?;
-    Ok(())
-}
 
 pub fn mmap_file(filepath: &str) -> Result<String> {
     use memmap2::Mmap;

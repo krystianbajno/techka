31688b8a446a5bbc82e2c2d7aee83e850a6a240e
k_b <krystian.bajno@gmail.com>
2025-05-27 13:36:15 +0200

    Update handler.py

--- plugins/file/handler.py
+++ plugins/file/handler.py
@@ -14,9 +14,9 @@ class Handler(Plugin):
 
         file_process_parser = file_subparsers.add_parser("process", help="Process a file")
         file_process_parser.add_argument("filepath", type=str, help="Path to the file to process")
-        file_process_parser.add_argument("--emails", action="store_true", help="Extract emails from the file (includes PDFs)")
-        file_process_parser.add_argument("--pdfs", action="store_true", help="Extract and print text from PDFs and documents")
-        file_process_parser.add_argument("--keywords", nargs="+", help="List of keywords to search for (includes PDFs)")
+        file_process_parser.add_argument("--emails", action="store_true", help="Extract emails from the file (supports PDF, DOCX, DOC, TXT)")
+        file_process_parser.add_argument("--pdfs", action="store_true", help="Extract and print text from documents (PDF, DOCX, DOC)")
+        file_process_parser.add_argument("--keywords", nargs="+", help="List of keywords to search for (supports PDF, DOCX, DOC, TXT)")
 
     def handle(self, args):
         action_map = {
@@ -38,12 +38,13 @@ class Handler(Plugin):
                 for pdf_text in get_pdfs(args.filepath):
                     print(pdf_text)
 
-            keywords = get_keywords(args.filepath, args.keywords)
-            for _1, _2, _3 in keywords:
-                colored_match = _3
-                for keyword in args.keywords:
-                    colored_match = colored_match.replace(keyword, f"{RED}{keyword}{RESET}")
-                print(f"[{YELLOW}{_1}{RESET}: {GREEN}{_2}{RESET}] {colored_match}")
+            if args.keywords:
+                keywords = get_keywords(args.filepath, args.keywords)
+                for _1, _2, _3 in keywords:
+                    colored_match = _3
+                    for keyword in args.keywords:
+                        colored_match = colored_match.replace(keyword, f"{RED}{keyword}{RESET}")
+                    print(f"[{YELLOW}{_1}{RESET}: {GREEN}{_2}{RESET}] {colored_match}")
                 
         else:
             print(f"File {args.filepath} does not exist.")
